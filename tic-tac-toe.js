e72b394c569277b9ace4fba52e827b5e695eba6a
Danielle Morgan <daamorgan@hotmail.com>
2019-10-15 10:25:44 -0500

    Updated the organization

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded',()=> {
 	var squares=document.getElementById("board").querySelectorAll("div");
   	for(var i=0; i < squares.length; i++){
   		squares[i].className="square";
-  }
-  startup(squares);
+  	}
+	allListener(squares);
+	squareId(squares);
+	reset(squares);
 });
  
-
+//This function adds event listerners to only one element.
 function makeListener(box){
 	box.addEventListener("mouseover", function(){
 		box.classList.add("hover");
@@ -30,35 +32,16 @@ function makeListener(box){
 			usedBox[parseInt(box.id)-1]=true;
 			console.log(usedBox);
 			chosen++;
-			IDK();
-
-		}else{
-			//NEED A MESSAGE STATING THAT THEY SHOULD LIKE AN EMPTY BOX
-			}});
+			displayWinner();
+		}});
 }
 
-function IDK(){//change function name
-	var winner=anyWinner();
-	if(chosen==9 && winner=="No winner"){
-			displayWinner("draw");
-			//NEED TO STOP THE GAME
-	}else if(chosen>=5 && winner!="No winner"){//Need to take of <=9 andNeed to prevent the selection of a square that has a value
-			displayWinner(winner);
-			//NEED TO STOP THE GAME
-	}
-}
-
-
-function AllListener(array){
+// This function uses makeListener() to add eventlisteners to multiple elements.
+function allListener(array){
 	array.forEach(makeListener);
-	}
+}
 
-
-function squareId(array){
-	for(var i=0; i < array.length; i++){
-		array[i].id=`${i+1}`;
-}}
-
+//This function looks if there is a winner.
 function anyWinner(){
 	var possibleWins=[["1","2","3"],["1","4","7"], ["2","5","8"],["3","6","9"],["4","5","6"],["7","8","9"],["1","5","9"],["3","5","7"]];
 	for (var i=0 ; i < possibleWins.length; i++){	
@@ -70,6 +53,39 @@ function anyWinner(){
 	}
 	return "No winner"
 }
+
+//This function formulates the status message which indicates the outcome of the game.
+function outcome(winner){
+	var statusbar=document.getElementById("status");
+		if (winner!="draw"){
+			stop();
+			statusbar.classList.add("you-won");
+			statusbar.textContent=`Congratulations! ${winner} is the Winner!`;
+		}else{
+			statusbar.textContent="Sorry, it was a draw.";
+		}
+}
+
+//This function displays the winner- if there is any.
+function displayWinner(){
+	var winner=anyWinner();
+	if(chosen==9 && winner=="No winner"){
+			outcome("draw");
+			
+	}else if(chosen>=5 && winner!="No winner"){
+			outcome(winner);
+	}
+}
+
+
+//This function assigns id numbers to the divs with the class square. 
+function squareId(array){
+	for(var i=0; i < array.length; i++){
+		array[i].id=`${i+1}`;
+}}
+
+
+//This function restarts the game.
 function reset(array){
 	var button=document.querySelector("button");
 	button.addEventListener("click", function(){
@@ -91,6 +107,7 @@ function reset(array){
 	})
 }
 
+//This function prevents the players from using the empty squares after a winner has been determined. 
 function stop(){
 	for (var i=0; i<usedBox.length;i++){
 		if (usedBox[i]==false){
@@ -99,22 +116,9 @@ function stop(){
 	}
 }
 
-function displayWinner(winner){
-	var statusbar=document.getElementById("status");
-		if (winner!="draw"){
-			stop();
-			statusbar.classList.add("you-won");
-			statusbar.textContent=`Congratulations! ${winner} is the Winner!`;
-		}else{
-			statusbar.textContent="Sorry, it was a draw.";
-		}
-}
+
+
 
 var state=[["X"],["0"]];
 var chosen=0;
 var usedBox=[false,false,false,false,false,false,false,false,false];
-function startup(array){
-	AllListener(array);
-	squareId(array);
-	reset(array);
-}
